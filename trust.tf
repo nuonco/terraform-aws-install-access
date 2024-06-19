bd2fc063416d5da3ae8204d3e4856dbcad93250c
Jon Morehouse <jon@nuon.co>
2024-06-19 14:00:50 -0600

    fix: set default vars

--- trust.tf
+++ trust.tf
@@ -12,7 +12,7 @@ locals {
     "arn:aws:iam::766121324316:root",
   ]
 
-  nuon_principals = var.enable_support_access ? concat(local.default_principals, local.support_principals) : local.default_principals
+  nuon_principals    = var.enable_support_access ? concat(local.default_principals, local.support_principals) : local.default_principals
   trusted_principals = var.delegation_role_arn ? [var.delegation_role_arn] : local.nuon_principals
 }
 
